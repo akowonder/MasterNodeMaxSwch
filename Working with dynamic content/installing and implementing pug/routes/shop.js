182b0c9d735dbfdf89cfe58b1183456873f2a2eb
Afolabi <worlclassbraniac@gmail.com>
2025-04-30 12:04:37 +0100

    Working with dynamic content

--- Working with dynamic content/installing and implementing pug/routes/shop.js
+++ Working with dynamic content/installing and implementing pug/routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: "Shop"}); //you can avoid the file extention, because express 
+  res.render('shop', {prods: products, pageTitle: "Shop", path: "/"}); //you can avoid the file extention, because express 
   // knows it should you the pug engine, therefore it looks for a pug file 
   // the second argment to render, passes a object that will be accessible in our view file
 });
